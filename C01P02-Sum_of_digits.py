f9b5da7086ddc371133b50627e30a64eb391d652
DenislavDyakov <denislav.dyakov@gmail.com>
2021-03-23 00:48:59 +0200

    Create C01P02-Sum_of_digits.py

---
+++ C01P02-Sum_of_digits.py
@@ -0,0 +1,28 @@
+'''
+Given an integer, implement a function, called sum_of_digits(n) that calculates the sum of n's digits.
+If a negative number is given, our function should work as if it was positive.
+'''
+def sum_of_digits(digits):
+
+     digits = str(digits)
+     list_digits = []
+     idx = 0
+
+     while idx < len(digits):
+          if digits[idx] == "-":
+               digit = digits[idx:idx+2]
+               list_digits.append(digit)
+               idx += 2
+          else:
+               digit = digits[idx]
+               list_digits.append(digit)
+               idx +=1
+
+     sum_digits = 0
+
+     for item in list_digits:
+          sum_digits += int(item)
+
+     return(sum_digits)
+
+print(sum_of_digits(-101325132435356)) # == 42
